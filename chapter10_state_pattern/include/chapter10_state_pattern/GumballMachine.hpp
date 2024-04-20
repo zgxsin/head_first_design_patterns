11ddfce53d92e8acb06dafe37111be2489caf5fe
Guoxiang Zhou <harigxzhou@gmail.com>
2024-04-21 01:08:50 +0200

    Add notes.

--- chapter10_state_pattern/include/chapter10_state_pattern/GumballMachine.hpp
+++ chapter10_state_pattern/include/chapter10_state_pattern/GumballMachine.hpp
@@ -35,6 +35,24 @@ namespace chapter10_state_pattern {
 
         int getNumberOfGumballs() const;
 
+        /**
+         * @brief This is an overload of the << operator for the GumballMachine class. The purpose of overloading this
+         * operator is to allow the GumballMachine class objects to be output to an output stream (std::ostream)
+         * using the familiar syntax std::cout << gumballMachine;
+         * @remark This method is a friend function. The friend keyword is used to declare this function
+         * as a friend of the GumballMachine class. A friend function is a function that is not a member of a class
+         * but has access to the private and protected members of that class.
+         *
+         * Declaration and Definition of Friend Functions:
+         * Friend functions are declared inside the class whose members they are allowed to access.
+         * Friend functions can be declared in the public, private, or protected section of the class.
+         * Friend functions are defined outside the class, just like any other non-member function.
+         * However, they have access to the private and protected members of the class.
+         *
+         * @param os The output stream
+         * @param gumballMachine  The GumballMachine object
+         * @return The output stream
+         */
         friend std::ostream &operator<<(std::ostream &os, const GumballMachine &gumballMachine);
 
     private:
